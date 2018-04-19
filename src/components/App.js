02611069b4b570c892d62931ad6a7b846e2730f1
Jim Lofgren <jim.lofgren.84@gmail.com>
2018-04-19 21:38:42 +0200

    google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 var React = require('react');
+var ReactGA = require('react-ga')
 var Instants = require('./Instants')
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            someData: null,
+        };
+        ReactGA.initialize('UA-117908076-1');
+        ReactGA.pageview(window.location.pathname);
+    };
+
     render() {
         return (
             <div className='container'>
